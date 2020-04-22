10e506e0b49fa02965e0a96e7abf9edbda2e3447
Chung Nguyen <nguyenthanhchung1502@gmail.com>
2020-04-22 16:01:52 +0700

    Add project files.

---
+++ WebApplication8/Models/BaseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class BaseDbContext : DbContext 
+    {
+        public BaseDbContext(DbContextOptions<BaseDbContext> contextOptions, IAuthorizationService authorizationService) : base(contextOptions)
+        {
+            _authorizationService = authorizationService;
+        }
+    }
+}
